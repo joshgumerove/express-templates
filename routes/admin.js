a99e214fb9500035dc84ce30578a430c0659c807
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-12 00:11:08 -0500

    install templating engines

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body); // will appear in the linux terminal (not shown in the browser since we are on the server)
   products.push({ title: req.body.title });
-  res.redirect('/');
+  console.log("look at products: ", products);
+  res.redirect("/");
 });
 
 exports.routes = router;
